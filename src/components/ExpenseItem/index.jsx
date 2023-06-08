791fd11eb48c2acb73399b09ab491218c3080ec2
jobe-lumni <jobecamera@gmail.com>
2023-06-08 20:15:00 -0300

    correcao nome do className='expense-item__description'

--- src/components/ExpenseItem/index.jsx
+++ src/components/ExpenseItem/index.jsx
@@ -5,7 +5,7 @@ export default function ExpenseItem() {
 	return (
 		<div className='expense-item'>
 			<div>June 6th 2023</div>
-			<div className='expense-item_description'>
+			<div className='expense-item__description'>
 				<h2>Car Insurance</h2>
 				<div className='expense-item__price'>$350.00</div>
 			</div>
