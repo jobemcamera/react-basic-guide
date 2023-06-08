81790fce6869a644f243a9dc80f19458f8c19bb9
jobe-lumni <jobecamera@gmail.com>
2023-06-08 20:59:49 -0300

    criacao do componente ExpenseDate

--- src/components/ExpenseItem/index.jsx
+++ src/components/ExpenseItem/index.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import './ExpenseItem.css'
+import ExpenseDate from '../ExpenseDate'
 
 export default function ExpenseItem({title, amount, date}) {
-	const month = date.toLocaleString('en-US', {month: 'long'});
-	const day = date.toLocaleString('en-US', {day: '2-digit'});
-	const year = date.getFullYear();
-
 	return (
 		<div className='expense-item'>
-			<div>
-				<div>{month}</div>
-				<div>{day}</div>
-				<div>{year}</div>
-			</div>
+			<ExpenseDate date={date}/>
 			<div className='expense-item__description'>
 				<h2>{title}</h2>
 				<div className='expense-item__price'>${amount}</div>
