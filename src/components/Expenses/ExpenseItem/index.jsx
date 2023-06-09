45897376fe583ee9715efcf83c7898008d0f4a07
jobe-lumni <jobecamera@gmail.com>
2023-06-09 18:33:52 -0300

    removido o botao de demonstracao de ExpenseItem

--- src/components/Expenses/ExpenseItem/index.jsx
+++ src/components/Expenses/ExpenseItem/index.jsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from '../ExpenseDate'
 import Card from '../../UI/Card'
 
 export default function ExpenseItem({title, amount, date}) {
-	
-	const [itemTitle, setItemTitle] = useState(title);
-
-	const clickHandler = () => {
-		setItemTitle('Ford');
-	}
-
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={date} />
 			<div className='expense-item__description'>
-				<h2>{itemTitle}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${amount}</div>
 			</div>
-			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	)
 }
