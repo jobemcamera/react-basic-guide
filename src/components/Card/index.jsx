6a517bee26c564eb4a0c1affff5f143078bee78a
jobe-lumni <jobecamera@gmail.com>
2023-06-08 21:18:43 -0300

    criacao do componente Card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Card.css'
+
+export default function Card({className, children}) {
+    const classes = 'card ' + className
+
+    return (
+        <div className={classes}>
+            {children}
+        </div>
+    )
+}
