ed7cc65bd87dc07819614ed1f1d0c62f37156976
jobe-lumni <jobecamera@gmail.com>
2023-06-09 18:46:50 -0300

    renderizando o componente ExpenseItem dinamicamente com o metodo map

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 	return (
 		<div>
 			<NewExpense onAddExpense={addExpenseHandler}/>
-			<Expenses expenses={expenses} />
+			<Expenses items={expenses} />
 		</div>
 	);
 }
--- src/components/Expenses/Expenses/index.jsx
+++ src/components/Expenses/Expenses/index.jsx
@@ -4,7 +4,7 @@ import Card from '../../UI/Card'
 import ExpenseItem from '../ExpenseItem'
 import ExpensesFilter from '../ExpensesFilter'
 
-export default function Expenses({ expenses }) {
+export default function Expenses({ items }) {
 
     const [filteredYear, setFilteredYear] = useState('2020');
 
@@ -15,15 +15,15 @@ export default function Expenses({ expenses }) {
     return (
         <>
             <Card className='expenses'>
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                <ExpenseItem
-                    title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-                <ExpenseItem
-                    title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-                <ExpenseItem
-                    title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-                <ExpenseItem
-                    title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                {items.map((item) => 
+                    <ExpenseItem 
+                        key={item.id}
+                        title={item.title} 
+                        amount={item.amount} 
+                        date={item.date} 
+                    />
+                )}
             </Card>
         </>
     )
