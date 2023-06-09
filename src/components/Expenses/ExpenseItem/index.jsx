326b2e8f51a228c3bfdf3097084a95ef7b1a30a0
jobe-lumni <jobecamera@gmail.com>
2023-06-09 08:50:13 -0300

    trabalhando com evento click e trocando dados com useState

--- src/components/Expenses/ExpenseItem/index.jsx
+++ src/components/Expenses/ExpenseItem/index.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from '../ExpenseDate'
 import Card from '../../UI/Card'
 
 export default function ExpenseItem({title, amount, date}) {
+	
+	const [itemTitle, setItemTitle] = useState(title);
+
+	const clickHandler = () => {
+		setItemTitle('Ford');
+	}
+
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={date} />
 			<div className='expense-item__description'>
-				<h2>{title}</h2>
+				<h2>{itemTitle}</h2>
 				<div className='expense-item__price'>${amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	)
 }
