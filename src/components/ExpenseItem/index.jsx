2c245454379d5efe43b01e121212a6706b24fdce
jobe-lumni <jobecamera@gmail.com>
2023-06-08 21:20:18 -0300

    refatorando ExpenseItem com o componente Card

--- src/components/ExpenseItem/index.jsx
+++ src/components/ExpenseItem/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from '../ExpenseDate'
+import Card from '../Card'
 
 export default function ExpenseItem({title, amount, date}) {
 	return (
-		<div className='expense-item'>
-			<ExpenseDate date={date}/>
+		<Card className='expense-item'>
+			<ExpenseDate date={date} />
 			<div className='expense-item__description'>
 				<h2>{title}</h2>
 				<div className='expense-item__price'>${amount}</div>
 			</div>
-		</div>
+		</Card>
 	)
 }
