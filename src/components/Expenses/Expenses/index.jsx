54242821b536004ac0fd108468eb6b9c0167df9c
jobe-lumni <jobecamera@gmail.com>
2023-06-09 22:14:02 -0300

    criacao dos componentes Chart e ChartBar

--- src/components/Expenses/Expenses/index.jsx
+++ src/components/Expenses/Expenses/index.jsx
@@ -3,25 +3,28 @@ import './Expenses.css'
 import Card from '../../UI/Card'
 import ExpensesFilter from '../ExpensesFilter'
 import ExpensesList from '../ExpensesList'
+import ExpensesChart from '../ExpensesChart'
 
-export default function Expenses({ items }) {
-
-    const [filteredYear, setFilteredYear] = useState('2023');
+const Expenses = ({ items }) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
 
-    const filteredExpenses = items.filter(item => {
+    const filteredExpenses = items.filter((item) => {
         return item.date.getFullYear().toString() === filteredYear;
     });
 
     return (
-        <>
+        <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                <ExpensesList items={filteredExpenses}/>
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
             </Card>
-        </>
-    )
-}
+        </div>
+    );
+};
+
+export default Expenses;
