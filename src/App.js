2520e3e1101f3f4b0051d611bf088996b614c8f7
jobe-lumni <jobecamera@gmail.com>
2023-06-10 19:30:49 -0300

    setando novo valor para ano do filtro

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ const DUMMY_EXPENSES = [
 	  amount: 94.12,
 	  date: new Date(2020, 7, 14),
 	},
-	{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+	{ id: 
+		'e2', 
+		title: 'New TV', 
+		amount: 799.49, 
+		date: new Date(2021, 2, 12) },
 	{
 	  id: 'e3',
 	  title: 'Car Insurance',
@@ -22,6 +26,12 @@ const DUMMY_EXPENSES = [
 	  amount: 450,
 	  date: new Date(2021, 5, 12),
 	},
+	{
+	  id: 'e5',
+	  title: 'New TV',
+	  amount: 950,
+	  date: new Date(2023, 6, 9),
+	}
   ];
 
 function App() {
